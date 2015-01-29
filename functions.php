f3a3d17adb3f43f2de58739fe616daac7bf81a10
Athar Hameed <atharh@gmail.com>
2015-01-29 16:42:49 +0800

    finished basic 3g flow

---
+++ functions.php
@@ -0,0 +1,134 @@
+<?php
+
+function toRedis($requestId, $params)
+{
+    $redis = new \LibSam\Cache\RedisCache();
+    $redis->set('netm::emulator::'.$requestId, json_encode($params), 3600);
+}
+
+function fromRedis($requestId)
+{
+    $redis = new \LibSam\Cache\RedisCache();
+    return json_decode($redis->get('netm::emulator::'.$requestId), true);
+}
+
+function PrepareInfo($requestId)
+{
+    $params = fromRedis($requestId);
+    $infoUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/netm-emulator/index.php/detectinfo';
+
+    $s = <<<HERE
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<Response type="PrepareInfo">
+  <StatusCode>0</StatusCode>
+  <StatusText>OK</StatusText>
+  <RequestID>${requestId}</RequestID>
+  <TransactionID>${requestId}</TransactionID>
+  <InfoURL>${infoUrl}</InfoURL>
+</Response>
+HERE;
+
+    return $s;
+}
+
+function PrepareSubscription($requestId)
+{
+    $params = fromRedis($requestId);
+    $infoUrl = 'http://' . $_SERVER['HTTP_HOST'] . '/netm-emulator/index.php/paymenturl?rid=' . $requestId;
+
+    if ($params['config']['low_balance']) {
+        $statusCode = 305;
+        $statusText = 'Low credit';
+    } else {
+        $statusCode = 0;
+        $statusText = 'OK';
+    }
+
+    $s = <<<HERE
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<Response type="PrepareSubscription">
+  <StatusCode>${statusCode}</StatusCode>
+  <StatusText>${statusText}</StatusText>
+  <TransactionType>synchronous</TransactionType>
+  <RequestID>${requestId}</RequestID>
+  <TransactionID>${requestId}</TransactionID>
+  <ValidityPeriod>216000</ValidityPeriod>
+  <PaymentURL>${infoUrl}</PaymentURL>
+</Response>
+HERE;
+
+    return $s;
+}
+
+function CheckTan($requestId)
+{
+    $params = fromRedis($requestId);
+
+    $s = <<<HERE
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<Response type="CheckTan">
+  <StatusCode>0</StatusCode>
+  <StatusText>PIN correct</StatusText>
+  <TransactionType>synchronous</TransactionType>
+  <RequestID>${requestId}</RequestID>
+  <TransactionID>${requestId}</TransactionID>
+</Response>
+HERE;
+
+    return $s;
+}
+
+function QueryInfo($requestId)
+{
+    $params = fromRedis($requestId);
+
+    $now = date('Y-m-d H:i:s');
+
+    if ($params['config']['flow'] == 'wap') {
+        $msisdn = $params['config']['msisdn'];
+    } else {
+        $msisdn = $params['Destination'];
+    }
+
+    if (!isset($params['Description'])) {
+        $params['Description'] = '';
+        $params['SubscriptionFee'] = '';
+    }
+
+    $s = <<<HERE
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<Response type="QueryInfo">
+  <StatusCode>0</StatusCode>
+  <StatusText>OK</StatusText>
+  <RequestID>${requestId}</RequestID>
+  <TransactionID>${requestId}</TransactionID>
+  <OperatorID>1</OperatorID>
+  <PaymentOperatorID>1</PaymentOperatorID>
+  <Description>{$params['Description']}</Description>
+  <Destination>${msisdn}</Destination>
+  <TransactionStatusCode>1</TransactionStatusCode>
+  <TransactionStatusText>Transmitted TAN to MS, waiting for TAN input</TransactionStatusText>
+  <Subscriptions>
+    <Number>1</Number>
+    <Subscription>
+      <SubscriptionID>${requestId}</SubscriptionID>
+      <SubscriptionStatusCode>1</SubscriptionStatusCode>
+      <SubscriptionStatusText>Subscription ready</SubscriptionStatusText>
+      <ServiceID>web_de_abo</ServiceID>
+      <ServiceType>web</ServiceType>
+      <SubscriptionFee>{$params['SubscriptionFee']}</SubscriptionFee>
+      <ItemFee>0</ItemFee>
+      <Currency>EURO-CENT</Currency>
+      <VAT>19.0</VAT>
+      <OperatorID>1</OperatorID>
+      <PaymentOperatorID>1</PaymentOperatorID>
+      <Description>{$params['Description']}</Description>
+      <StartTimestamp>${now}</StartTimestamp>
+      <LastPeriodFeeTimestamp>${now}</LastPeriodFeeTimestamp>
+    </Subscription>
+  </Subscriptions>
+</Response>
+HERE;
+
+    return $s;
+}
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+require_once 'functions.php';
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,6 +23,7 @@ $requestTypes = array(
     'PrepareSubscription' => array(
         'Category',
         'ChargeFirstFee',
+        'ConfirmationURL',
         'Currency',
         'Description',
         'Destination',
@@ -66,6 +68,7 @@ $requestTypes = array(
         'RequestType',
         'ServiceID',
         'ServiceType',
+        'TransactionID',
         'Username',
         'Version',
     ),
@@ -75,115 +78,66 @@ $keyPrefix = "netm::emulator::";
 
 $app->match('/transport/PaymentGateway', function (Request $request) use ($requestTypes, $keyPrefix) {
     $requestId = $request->get('RequestID');
+
     $requestType = $request->get('RequestType');
+
+    if (!$requestType || !isset($requestTypes[$requestType])) {
+        throw new Exception(sprintf("$requestType missing"));
+    }
 
     $params = array();
     $params['TransactionID'] = $requestId;
+
+    //$params['config'] = json_decode(base64_decode($requestId, true), true);
+    $params['config'] = array();
+    $params['config']['low_balance'] = false;
+    $params['config']['flow'] = 'wap';
+    $params['config']['msisdn'] = '00491711049395';
 
     foreach ($requestTypes[$requestType] as $param) {
         $params[$param] = $request->get($param);
     }
 
-    $redis = new \LibSam\Cache\RedisCache();
-    $redis->set($keyPrefix.$requestId, json_encode($params));
-
-
-    if ($requestType == 'PrepareInfo') {
-        $s = <<<HERE
-<?xml version="1.0" encoding="ISO-8859-1"?>
-<Response type="PrepareInfo">
-  <StatusCode>0</StatusCode>
-  <StatusText>OK</StatusText>
-  <RequestID>${requestId}</RequestID>
-  <TransactionID>${requestId}</TransactionID>
-  <InfoURL>http://172.19.0.2/netm-emulator/index.php/detectinfo</InfoURL>
-</Response>
-HERE;
-        return new Response($s, 200, array('Content-Type' => 'application/xml'));
-    } else if ($requestType == 'PrepareSubscription') {
-
-        $s = <<<HERE
-<?xml version="1.0" encoding="ISO-8859-1"?>
-<Response type="PrepareSubscription">
-  <StatusCode>0</StatusCode>
-  <StatusText>OK</StatusText>
-  <TransactionType>synchronous</TransactionType>
-  <RequestID>${requestId}</RequestID>
-  <TransactionID>${requestId}</TransactionID>
-  <ValidityPeriod>216000</ValidityPeriod>
-  <PaymentURL>http://pgw.wap.net-m.net/pgw/io/cp/detect/1511111254/31dd9a2130825406be857a71ef0abdb3</PaymentURL>
-</Response>
-HERE;
-
-        return new Response($s, 200, array('Content-Type' => 'application/xml'));
-    } else if ($requestType == 'CheckTan') {
-
-        $s = '<?xml version="1.0" encoding="ISO-8859-1"?>
-<Response type="CheckTan">
-  <StatusCode>0</StatusCode>
-  <StatusText>PIN correct</StatusText>
-  <TransactionType>synchronous</TransactionType>
-  <RequestID>${requestId}</RequestID>
-  <TransactionID>${requestId}</TransactionID>
-</Response>';
-
-        return new Response($s, 200, array('Content-Type' => 'application/xml'));
-    } else if ($requestType == 'QueryInfo') {
-
-        $s = '<?xml version="1.0" encoding="ISO-8859-1"?>
-<Response type="QueryInfo">
-  <StatusCode>0</StatusCode>
-  <StatusText>OK</StatusText>
-  <RequestID>2362e55c</RequestID>
-  <TransactionID>P1408202022</TransactionID>
-  <OperatorID>1</OperatorID>
-  <PaymentOperatorID>1</PaymentOperatorID>
-  <Description>SUPERBATTERY</Description>
-  <Destination>00491711049392</Destination>
-  <TransactionStatusCode>1</TransactionStatusCode>
-  <TransactionStatusText>Transmitted TAN to MS, waiting for TAN input</TransactionStatusText>
-  <Subscriptions>
-    <Number>1</Number>
-    <Subscription>
-      <SubscriptionID>S0466154956</SubscriptionID>
-      <SubscriptionStatusCode>1</SubscriptionStatusCode>
-      <SubscriptionStatusText>Subscription ready</SubscriptionStatusText>
-      <ServiceID>web_de_abo</ServiceID>
-      <ServiceType>web</ServiceType>
-      <SubscriptionFee>699</SubscriptionFee>
-      <ItemFee>0</ItemFee>
-      <Currency>EURO-CENT</Currency>
-      <VAT>19.0</VAT>
-      <OperatorID>1</OperatorID>
-      <PaymentOperatorID>1</PaymentOperatorID>
-      <Description>BEEMOBI - SWINGCOPTERS</Description>
-      <StartTimestamp>2014-12-22 05:19:16</StartTimestamp>
-      <LastPeriodFeeTimestamp>2014-12-22 05:19:53</LastPeriodFeeTimestamp>
-    </Subscription>
-  </Subscriptions>
-</Response>';
-
-        return new Response($s, 200, array('Content-Type' => 'application/xml'));
+    $prevParams = fromRedis($requestId);
+    if ($prevParams) {
+        $params = array_merge($prevParams, $params);
     }
 
+    toRedis($requestId, $params);
 
-    return new Response('hello world');
+    return new Response($requestType($requestId), 200, array('Content-Type' => 'application/xml'));
 });
 
 $app->match('/detectinfo', function (Request $request) use ($requestTypes, $keyPrefix) {
-    $redis = new \LibSam\Cache\RedisCache();
-    $requestId = $request->get('rid');
-    $params = json_decode($redis->get($keyPrefix.$requestId), true);
+    $params = fromRedis($request->get('rid'));
+
+    if ($params['config']['flow'] == 'wap') {
+        $code = '0';
+    } else {
+        $code = '151';
+    }
 
     return new RedirectResponse($params['CustomerURL'].'?'.http_build_query(
         array(
             'trid' => $params['TransactionID'],
-            'code' => '151',
+            'code' => $code,
             'rid' => $params['RequestID'],
         )
     ));
 });
 
+$app->match('/paymenturl', function (Request $request) use ($requestTypes, $keyPrefix) {
+    $requestId = $request->get('rid');
+    $params = fromRedis($requestId);
+
+    $confirmationURL = 'http://172.19.0.2' . parse_url("http://mcb-test.sam-media.com:8082/mcb-ads/optin2.php/SWINGCOPTERS", PHP_URL_PATH);
+
+    $contents = file_get_contents($confirmationURL);
+    $url = $params['ProductURL'] . '?rid=' . $requestId;
+    $contents = str_replace('$PRODUCT_URL', $url, $contents);
+
+    return new Response($contents);
+});
 
 $app->match('/rid', function (Request $request) use ($requestTypes, $keyPrefix) {
     $redis = new \LibSam\Cache\RedisCache();
